896af49a8c9ec5e1ec8681a42df8c20257477d40
futurecodee <150053824+futurecodee@users.noreply.github.com>
2023-11-06 22:18:21 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+
+<form>
+<div class="container-md">
+  <div class="row justify-content-center">
+    <div class="card border-2 border-black shadow-lg" style="width: 35rem; margin-top:180px;">
+      <div class="card-body">
+        <h2 class="text-center mb-5 mt-4">MASUK</h2>
+        
+        <!-- Content here -->
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Alamat Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">Kami Tidak Akan Share Akun Anda.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+      <button type="submit" class="btn btn-primary"><a href="index.php" style="text-decoration: none; color:white;">login</a></button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</html> 
